c6098064eb88e78b7085510a7c921f8183ecc21c
AlexanderPB20 <preciadoalexander647@gmail.com>
2026-02-09 11:30:30 +0100

    Update game.h

--- pprog_26/game.h
+++ pprog_26/game.h
@@ -57,23 +57,89 @@ Status game_destroy(Game *game);
  * @return the space where the space_id coincide
  */
 Space *game_get_space(Game *game, Id space_id);
-
+/**
+ * @brief It gets the current location of the player
+ * @author Profesores PPROG
+ *
+ * @param game a pointer to the game
+ * @return the ID of the space where the player is located
+ */
 Id game_get_player_location(Game *game);
 
+/**
+ * @brief It sets the current location of the player
+ * @author Profesores PPROG
+ *
+ * @param game a pointer to the game
+ * @param id the ID of the space for the player's new location
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
 Status game_set_player_location(Game *game, Id id);
 
+/**
+ * @brief It gets the current location of the object
+ * @author Profesores PPROG
+ *
+ * @param game a pointer to the game
+ * @return the ID of the space where the object is located
+ */
 Id game_get_object_location(Game *game);
 
+/**
+ * @brief It sets the location of the object
+ * @author Profesores PPROG
+ *
+ * @param game a pointer to the game
+ * @param space_id the ID of the space for the object's new location
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
 Status game_set_object_location(Game *game, Id space_id);
 
+/**
+ * @brief It gets the last command executed in the game
+ * @author Profesores PPROG
+ *
+ * @param game a pointer to the game
+ * @return a pointer to the last command structure
+ */
 Command* game_get_last_command(Game *game);
 
+/**
+ * @brief It sets the last command executed in the game
+ * @author Profesores PPROG
+ *
+ * @param game a pointer to the game
+ * @param command a pointer to the command structure to be set
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
 Status game_set_last_command(Game *game, Command *command);
 
+/**
+ * @brief It checks if the game is finished
+ * @author Profesores PPROG
+ *
+ * @param game a pointer to the game
+ * @return TRUE if the game is over, FALSE otherwise
+ */
 Bool game_get_finished(Game *game);
 
+/**
+ * @brief It sets the finished state of the game
+ * @author Profesores PPROG
+ *
+ * @param game a pointer to the game
+ * @param finished the new state of the finished flag
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
 Status game_set_finished(Game *game, Bool finished);
 
+/**
+ * @brief It prints the game information for debugging
+ * @author Profesores PPROG
+ *
+ * @param game a pointer to the game to be printed
+ */
 void game_print(Game *game);
 
 #endif
+
