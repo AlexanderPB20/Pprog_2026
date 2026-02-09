99115a00cc337cb378a3cf43f9c96fd69520be85
elhereu <el.hereu204@gmail.com>
2026-02-09 11:24:02 +0100

    Update game_actions.c

--- pprog_26/game_actions.c
+++ pprog_26/game_actions.c
@@ -1,148 +1,157 @@
 /**
- * @brief It implements the game update through user actions
+ * @brief It implements a textual graphic engine
  *
- * @file game.c
+ * @file graphic_engine.c
  * @author Profesores PPROG
  * @version 0
- * @date 27-01-2025
+ * @date 24-01-2026
  * @copyright GNU Public License
  */
 
-#include "game_actions.h"
+#include "graphic_engine.h"
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-/**
-   Private functions
-*/
+#include "command.h"
+#include "libscreen.h"
+#include "space.h"
+#include "types.h"
 
-void game_actions_unknown(Game *game);
+#define WIDTH_MAP 48
+#define WIDTH_DES 29
+#define WIDTH_BAN 25
+#define HEIGHT_MAP 13
+#define HEIGHT_BAN 1
+#define HEIGHT_HLP 2
+#define HEIGHT_FDB 3
 
-void game_actions_exit(Game *game);
+struct _Graphic_engine {
+  Area *map, *descript, *banner, *help, *feedback;
+};
 
-void game_actions_next(Game *game);
+Graphic_engine *graphic_engine_create() {
+  static Graphic_engine *ge = NULL;
 
-void game_actions_back(Game *game);
-
-void game_actions_take(Game *game);
-
-void game_actions_drop(Game* game);
-
-/**
-   Game actions implementation
-*/
-
-Status game_actions_update(Game *game, Command *command) {
-  CommandCode cmd;
-
-  game_set_last_command(game, command);
-
-  cmd = command_get_code(command);
-
-  switch (cmd) {
-    case UNKNOWN:
-      game_actions_unknown(game);
-      break;
-
-    case EXIT:
-      game_actions_exit(game);
-      break;
-
-    case NEXT:
-      game_actions_next(game);
-      break;
-
-    case BACK:
-      game_actions_back(game);
-      break;
-
-    case TAKE:
-      game_actions_take(game);
-      break;
-
-    case DROP:
-      game_actions_drop(game);
-      break;
-
-    default:
-      break;
+  if (ge) {
+    return ge;
   }
 
-  return OK;
+  screen_init(HEIGHT_MAP + HEIGHT_BAN + HEIGHT_HLP + HEIGHT_FDB + 4, WIDTH_MAP + WIDTH_DES + 3);
+  ge = (Graphic_engine *)calloc(1, sizeof(Graphic_engine));
+  if (ge == NULL) {
+    return NULL;
+  }
+
+  ge->map = screen_area_init(1, 1, WIDTH_MAP, HEIGHT_MAP);
+  ge->descript = screen_area_init(WIDTH_MAP + 2, 1, WIDTH_DES, HEIGHT_MAP);
+  ge->banner = screen_area_init((int)((WIDTH_MAP + WIDTH_DES + 1 - WIDTH_BAN) / 2), HEIGHT_MAP + 2, WIDTH_BAN, HEIGHT_BAN);
+  ge->help = screen_area_init(1, HEIGHT_MAP + HEIGHT_BAN + 2, WIDTH_MAP + WIDTH_DES + 1, HEIGHT_HLP);
+  ge->feedback = screen_area_init(1, HEIGHT_MAP + HEIGHT_BAN + HEIGHT_HLP + 3, WIDTH_MAP + WIDTH_DES + 1, HEIGHT_FDB);
+
+  return ge;
 }
 
-/**
-   Calls implementation for each action
-*/
+void graphic_engine_destroy(Graphic_engine *ge) {
+  if (!ge) return;
 
-void game_actions_unknown(Game *game) {}
+  screen_area_destroy(ge->map);
+  screen_area_destroy(ge->descript);
+  screen_area_destroy(ge->banner);
+  screen_area_destroy(ge->help);
+  screen_area_destroy(ge->feedback);
 
-void game_actions_exit(Game *game) {}
-
-void game_actions_next(Game *game) {
-  Id current_id = NO_ID;
-  Id space_id = NO_ID;
-
-  space_id = game_get_player_location(game);
-  if (space_id == NO_ID) {
-    return;
-  }
-
-  current_id = space_get_south(game_get_space(game, space_id));
-  if (current_id != NO_ID) {
-    game_set_player_location(game, current_id);
-  }
-
-  return;
+  screen_destroy();
+  free(ge);
 }
 
-void game_actions_back(Game *game) {
-  Id current_id = NO_ID;
-  Id space_id = NO_ID;
+void graphic_engine_paint_game(Graphic_engine *ge, Game *game) {
+  Id id_act = NO_ID, id_back = NO_ID, id_next = NO_ID, obj_loc = NO_ID;
+  Space *space_act = NULL;
+  char obj = '\0';
+  char str[255];
+  CommandCode last_cmd = UNKNOWN;
+  extern char *cmd_to_str[N_CMD][N_CMDT];
 
-  space_id = game_get_player_location(game);
+  /* Paint the in the map area */
+  screen_area_clear(ge->map);
+  if ((id_act = game_get_player_location(game)) != NO_ID) {
+    space_act = game_get_space(game, id_act);
+    id_back = space_get_north(space_act);
+    id_next = space_get_south(space_act);
 
-  if (NO_ID == space_id) {
-    return;
+    if (game_get_object_location(game) == id_back)
+      obj = '+';
+    else
+      obj = ' ';
+
+    if (id_back != NO_ID) {
+      sprintf(str, "  |         %2d|", (int)id_back);
+      screen_area_puts(ge->map, str);
+      sprintf(str, "  |     %c     |", obj);
+      screen_area_puts(ge->map, str);
+      sprintf(str, "  +-----------+");
+      screen_area_puts(ge->map, str);
+      sprintf(str, "        ^");
+      screen_area_puts(ge->map, str);
+    }
+
+    if (game_get_object_location(game) == id_act)
+      obj = '+';
+    else
+      obj = ' ';
+
+    if (id_act != NO_ID) {
+      sprintf(str, "  +-----------+");
+      screen_area_puts(ge->map, str);
+      sprintf(str, "  | ^C>     %2d|", (int)id_act);
+      screen_area_puts(ge->map, str);
+      sprintf(str, "  |     %c     |", obj);
+      screen_area_puts(ge->map, str);
+      sprintf(str, "  +-----------+");
+      screen_area_puts(ge->map, str);
+    }
+
+    if (game_get_object_location(game) == id_next)
+      obj = '+';
+    else
+      obj = ' ';
+
+    if (id_next != NO_ID) {
+      sprintf(str, "        v");
+      screen_area_puts(ge->map, str);
+      sprintf(str, "  +-----------+");
+      screen_area_puts(ge->map, str);
+      sprintf(str, "  |         %2d|", (int)id_next);
+      screen_area_puts(ge->map, str);
+      sprintf(str, "  |     %c     |", obj);
+      screen_area_puts(ge->map, str);
+    }
   }
 
-  current_id = space_get_north(game_get_space(game, space_id));
-  if (current_id != NO_ID) {
-    game_set_player_location(game, current_id);
+  /* Paint in the description area */
+  screen_area_clear(ge->descript);
+  if ((obj_loc = game_get_object_location(game)) != NO_ID) {
+    sprintf(str, "  Object location:%d", (int)obj_loc);
+    screen_area_puts(ge->descript, str);
   }
 
-  return;
-}
+  /* Paint in the banner area */
+  screen_area_puts(ge->banner, " The haunted castle game ");
 
-void game_actions_take(Game *game) {
-  
-  if (!game)
-  {
-    return;
-  }
+  /* Paint in the help area */
+  screen_area_clear(ge->help);
+  sprintf(str, " The commands you can use are:");
+  screen_area_puts(ge->help, str);
+  sprintf(str, "     next or n, back or b, exit or e, take or t, drop or d");
+  screen_area_puts(ge->help, str);
 
-  if (space_get_object(game_get_space(game, game_get_player_location(game))) != NO_ID) // si en la sala en la que esta player, el objeto existe (no es NO_ID)
-  {
-    player_set_object(game->player, object_get_id(game->object)); // jugador toma objeto
-    space_set_object(game_get_space(game, game_get_player_location(game)), NO_ID); // sala vacia
-  }
-  return;
-}
+  /* Paint in the feedback area */
+  last_cmd = command_get_code(game_get_last_command(game));
+  sprintf(str, " %s (%s)", cmd_to_str[last_cmd - NO_CMD][CMDL], cmd_to_str[last_cmd - NO_CMD][CMDS]);
+  screen_area_puts(ge->feedback, str);
 
-void game_actions_drop(Game* game) {
-
-  if (!game)
-  {
-    return;
-  }
-
-  if (player_get_object(game->player) != NO_ID && space_get_object(game_get_space(game, game_get_player_location(game)))) // player tiene objeto y espacio vacio
-  {
-    space_set_object(game_get_space(game, game_get_player_location(game)), object_get_id(game->object)); // la sala tiene el objeto
-    player_set_object(game->player, NO_ID); // jugador deja el objeto
-  }
-
-  return;
+  /* Dump to the terminal */
+  screen_paint();
+  printf("prompt:> ");
 }
