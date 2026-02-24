d9371bd9a8cb67293e1a5e6970b35dec355fd395
elhereu <el.hereu204@gmail.com>
2026-02-24 12:05:40 +0100

    Update set.h

--- pprog_26/set.h
+++ pprog_26/set.h
@@ -17,26 +17,71 @@ typedef struct _Set Set;
 #include "types.h"
 
 /**
- * @brief It creates a new object, allocating memory and initializing its members
+ * @brief It creates a new set, allocating memory and initializing its members
  * @author Samuel Manzorro
  *
- * @param id the identification number for the new object
- * @return a new object, initialized
+ * @return a new set, initialized
  */
 Set *set_create();
 
 /**
- * @brief It destroys a object, freeing the allocated memory
+ * @brief It destroys a set, freeing the allocated memory
  * @author Samuel Manzorro
  *
- * @param object a pointer to the object that must be destroyed
+ * @param set a pointer to the set that must be destroyed
  * @return OK, if everything goes well or ERROR if there was some mistake
  */
 Status set_destroy(Set *set);
 
+/**
+* @brief It adds the id parameter to the set
+* @author Samuel Manzorro
+*
+* @param set a pointer to the set
+* @param id the id of the element to be added to the set
+* @return OK, if everything goes well or ERROR if there was some mistake
+*/
 Status add_element_to_set(Set *set, Id id);
 
+/**
+* @brief It deletes the id parameter to the set
+* @author Samuel Manzorro
+*
+* @param set a pointer to the set
+* @param id the id of the element to be deleted from the set
+* @return OK, if everything goes well or ERROR if there was some mistake
+*/
+Status del_element_to_set(Set *set, Id id);
+
+/**
+ * @brief It gets the id from the position asked
+ * @author Profesores PPROG
+ *
+ * @param set a pointer to the set
+ * @param i the position of the id searched
+ * @return the id asked
+ */
+Id set_get_element(Set *set, int i);
+
+/**
+ * @brief It gets the number of ids of the set
+ * @author Profesores PPROG
+ *
+ * @param set a pointer to the set
+ * @return the number of ids
+ */
+int set_get_n_elements(Set *set);
+
+/**
+ * @brief It prints the set information
+ * @author Profesores PPROG
+ *
+ * This function shows the number of ids there is and the list of ids
+ * @param set a pointer to the set
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
 Status set_print(Set* set);
 
 
 #endif
+
