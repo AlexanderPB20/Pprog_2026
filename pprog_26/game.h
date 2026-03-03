21d46aecd467d5990fc2c0e5a701b10a9e6160ec
elhereu <el.hereu204@gmail.com>
2026-03-03 13:10:55 +0100

    Update game.h

--- pprog_26/game.h
+++ pprog_26/game.h
@@ -16,16 +16,23 @@
 #include "types.h"
 #include "object.h"
 #include "player.h"
+#include "character.h"
 
 #define MAX_SPACES 100
+#define MAX_OBJECTS 100
+#define MAX_PLAYERS 100
+#define MAX_CHARACTERS 100
 
 typedef struct _Game {
-  Object* object;
-  Player* player;
-  Space *spaces[MAX_SPACES];
-  int n_spaces;
-  Command *last_cmd;
-  Bool finished;
+  Object* objects[MAX_OBJECTS]; // array of objects that contains the game
+  int n_objects; // number of objects the game has
+  Player *player; // the player plau¡ying the game
+  Space *spaces[MAX_SPACES]; // array of spaces that contains all the spaces of the game
+  int n_spaces; // number of valid spaces the game map has
+  Character *characters[MAX_SPACES]; // array of characters that contains the game
+  int n_characters; // number of characters the game has
+  Command *last_cmd; // last command typed by the user
+  Bool finished; // true if the game us finished, false if not
 } Game;
 
 /**
@@ -35,16 +42,7 @@ typedef struct _Game {
  * @param game the identification number for the new game
  * @return a new game, initialized
  */
-Status game_create(Game *game);
-/**
- * @brief It creates a new game fromm filename
- * @author Samuel Manzorro
- *
- * @param game the identification number for the new game
- * @param filename a pointer to charfilename with spaceloaded
- * @return a new game, initialized
- */
-Status game_create_from_file(Game *game, char *filename);
+Game *game_create();
 
 /**
  * @brief It destroys a game, freeing the allocated memory
@@ -56,14 +54,109 @@ Status game_create_from_file(Game *game, char *filename);
 Status game_destroy(Game *game);
 
 /**
- * @brief It gets the space from
+ * @brief It gets the space where the object id space
  * @author Samuel Manzorro
  *
- * @param game a pointer to the game that must be destroyed
+ * @param game a pointer to the game
  * @param space_id the id of the space
  * @return the space where the space_id coincide
  */
 Space *game_get_space(Game *game, Id space_id);
+
+/**
+ * @brief it gets the number of spaces in the game
+ * @author Samuel Manzorro
+ * 
+ * @param game a pointer to the game
+ * @return the number of spaces in the game
+ *  */
+int game_get_n_spaces(Game *game);
+
+/**
+ * @brief It gets the object where the object id coincides
+ * @author Samuel Manzorro
+ *
+ * @param game a pointer to the game
+ * @param object_id the id of the object
+ * @return the object where the object id coincides
+ */
+Object *game_get_object(Game *game, Id object_id);
+
+/**
+ * @brief it gets the number of objects in the game
+ * @author Samuel Manzorro
+ * 
+ * @param game a pointer to the game
+ * @return the number of objects in the game
+ *  */
+int game_get_n_objects(Game *game);
+
+/**
+ * @brief It gets the player of the game
+ * @author Samuel Manzorro
+ *
+ * @param game a pointer to the game
+ * @return the player of the game
+ */
+Player *game_get_player(Game *game);
+
+/**
+ * @brief It sets the player of the game
+ * @author Samuel Manzorro
+ *
+ * @param game a pointer to the game
+ * @param player the player to be set
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
+Status game_set_player(Game *game, Player *player);
+
+/**
+ * @brief It gets the character where the character id coincides
+ * @author Samuel Manzorro
+ *
+ * @param game a pointer to the game
+ * @param character_id the id of the character
+ * @return the character where the character id coincides
+ */
+Character *game_get_character(Game *game, Id character_id);
+
+/**
+ * @brief it gets the number of characters in the game
+ * @author Samuel Manzorro
+ * 
+ * @param game a pointer to the game
+ * @return the number of characters in the game
+ *  */
+int game_get_n_characters(Game *game);
+
+
+
+/**
+ * @brief Añade un espacio a la estructura del juego
+ * @author Alexander Preciado
+ *
+ * Comprueba que el espacio no sea nulo y que no se haya superado el límite
+ * máximo antes de añadirlo al array de espacios del juego.
+ *
+ * @param game Puntero a la estructura del juego.
+ * @param space Puntero al espacio que se desea añadir.
+ * @return OK si se añade con éxito, ERROR si hay fallo o el juego está lleno.
+ */
+Status game_add_space(Game *game, Space *space);
+
+/**
+ * @brief Obtiene el ID del espacio en una posición determinada
+ * @author Alexander Preciado
+ *
+ * Permite acceder al identificador de un espacio basándose en su posición
+ * dentro del array interno del juego.
+ *
+ * @param game Puntero a la estructura del juego.
+ * @param position Índice del espacio en el array de espacios del juego.
+ * @return Id del espacio si la posición es válida, NO_ID en caso contrario.
+ */
+Id game_get_space_id_at(Game *game, int position);
+
 /**
  * @brief It gets the current location of the player
  * @author Profesores PPROG
@@ -88,19 +181,21 @@ Status game_set_player_location(Game *game, Id id);
  * @author Profesores PPROG
  *
  * @param game a pointer to the game
+ * @param object a pointer to the object
  * @return the ID of the space where the object is located
  */
-Id game_get_object_location(Game *game);
+Id game_get_object_location(Game *game, Object* object);
 
 /**
  * @brief It sets the location of the object
  * @author Samuel Manzorro
  *
  * @param game a pointer to the game
+ * @param object a pointer to the object
  * @param space_id the ID of the space for the object's new location
  * @return OK, if everything goes well or ERROR if there was some mistake
  */
-Status game_set_object_location(Game *game, Id space_id);
+Status game_set_object_location(Game *game, Object* object, Id space_id);
 
 /**
  * @brief It gets the last command executed in the game
@@ -149,5 +244,11 @@ Status game_set_finished(Game *game, Bool finished);
 void game_print(Game *game);
 
 #endif
+
+
+
+
+#endif
+
 
 
