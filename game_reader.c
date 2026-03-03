0c2d17afdb8a879aa67be632efd270d5a1688d71
AlexanderPB20 <preciadoalexander647@gmail.com>
2026-03-03 12:28:27 +0100

    Update game_reader.c

--- game_reader.c
+++ game_reader.c
@@ -9,7 +9,6 @@
  */
 
 #include "game_reader.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,6 +18,8 @@
 */
 
 Status game_reader_create_from_file(Game *game, char *filename) {
+  Player *player = NULL;
+
   if (game_create(game) == ERROR) {
     return ERROR;
   }
@@ -27,13 +28,19 @@ Status game_reader_create_from_file(Game *game, char *filename) {
     return ERROR;
   }
 
-  /* The player and the object are located in the first space */
-  game_set_player_location(game, game_get_space_id_at(game, 0));
-  game_set_object_location(game, game_get_space_id_at(game, 0));
+  if (game_reader_load_objects(game, filename) == ERROR) {
+    return ERROR;
+  }
+
+  player = game_get_player(game, 1);
+  if (player != NULL) {
+    /* le pasamos el puntero al jugador como segundo argumento */
+    game_set_player_location(game, player, game_get_space_id_at(game, 0));
+  }
+
 
   return OK;
 }
-
 
 /**
    Implementation of private functions
@@ -100,53 +107,45 @@ Status game_reader_load_objects(Game *game, char *filename) {
   char line[WORD_SIZE] = "";
   char name[WORD_SIZE] = "";
   char *toks = NULL;
-  Id id = NO_ID, north = NO_ID, east = NO_ID, south = NO_ID, west = NO_ID;
-  Space *space = NULL;
-  Status status = OK;
+  Id id = NO_ID, space_id = NO_ID;
+  Object *obj = NULL;
 
-  if (!filename) {
-    return ERROR;
-  }
+  if (!filename || !game) return ERROR;
 
   file = fopen(filename, "r");
-  if (file == NULL) {
-    return ERROR;
-  }
+  if (!file) return ERROR;
 
   while (fgets(line, WORD_SIZE, file)) {
-    if (strncmp("#s:", line, 3) == 0) {
+    if (strncmp("#o:", line, 3) == 0) {
+      /* Extraer ID */
       toks = strtok(line + 3, "|");
+      if (!toks) continue;
       id = atol(toks);
+
+      /* Extraer Nombre */
       toks = strtok(NULL, "|");
+      if (!toks) continue;
       strcpy(name, toks);
+
+      /* Extraer ID del espacio */
       toks = strtok(NULL, "|");
-      north = atol(toks);
-      toks = strtok(NULL, "|");
-      east = atol(toks);
-      toks = strtok(NULL, "|");
-      south = atol(toks);
-      toks = strtok(NULL, "|");
-      west = atol(toks);
-#ifdef DEBUG
-      printf("Leido: s:%ld|%s|%ld|%ld|%ld|%ld\n", id, name, north, east, south, west);
-#endif
-      space = space_create(id);
-      if (space != NULL) {
-        space_set_name(space, name);
-        space_set_north(space, north);
-        space_set_east(space, east);
-        space_set_south(space, south);
-        space_set_west(space, west);
-        game_add_space(game, space);
+      if (!toks) continue;
+      space_id = atol(toks);
+
+      /* Crear el objeto y añadirlo al juego */
+      obj = object_create(id);
+      if (obj != NULL) {
+        object_set_name(obj, name);
+        game_add_object(game, obj);
+        game_set_object_location(game, obj, space_id);
       }
     }
   }
 
-  if (ferror(file)) {
-    status = ERROR;
-  }
-
   fclose(file);
+  return OK;
+}
 
   return status;
+
 }
